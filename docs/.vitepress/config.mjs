6db7dbd411728374bee3b88e15f819d09db8eea5
dayu <wujy1025@126.com>
2024-02-21 13:57:39 +0800

    init

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,71 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: "/jywud/",
+  title: "大鱼的空间",
+  description: "大鱼的个人博客",
+  // head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    // logo: '/logo.svg',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '指南', link: '/guide/markdown-examples' },
+      { text: '测试', link: '/more/test' },
+      {
+        text: '更多',
+        items: [
+          { text: '百度', link: 'https://www.baidu.com' }
+        ]
+      }
+    ],
+
+    sidebar: {
+      '/guide/': [
+        {
+          text: 'Examples1',
+          collapsed: true,
+          items: [
+            { text: 'Markdown Examples', link: '/guide/markdown-examples' },
+            { text: 'Runtime API Examples', link: '/guide/api-examples' }
+          ]
+        }
+      ],
+      '/more/': [
+        {
+          text: 'Test',
+          items: [
+            { text: 'test', link: '/more/test' },            
+          ]
+        }
+      ]
+    },
+    // [
+    //   {
+    //     text: 'Examples1',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://gitee.com/jywud314' }
+    ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2024-present dayu'
+    },
+
+    lastUpdated: {
+      text: 'Updated at',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+      }
+    }
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: 大鱼的空间主页
+  text: 有很多的学习资料
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: guide
+      link: /guide/markdown-examples
+    - theme: alt
+      text: test Examples
+      link: /more/test
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
